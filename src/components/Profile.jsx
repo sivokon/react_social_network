c0199b6368a2959feefa4415200c99e93216cac8
Artem Tarasenko-Syvokon <Artem_Tarasenko-Syvokon@epam.com>
2020-02-16 23:39:06 +0200

    all project files added, App, Header, Navbar and Profile components added

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://schuttejoe.github.io/img/Posts/DisneyPostMortem/Preview.png" />
+      </div>
+      <div>
+        avatar + description
+        </div>
+      <div>
+        my posts
+          <div>
+          new post
+          </div>
+        <div>
+          post 1
+          </div>
+        <div>
+          post 2
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
