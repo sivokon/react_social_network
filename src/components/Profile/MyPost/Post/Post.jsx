2d3b39ec34765340ee889c3406a148034a63a07e
Artem Tarasenko-Syvokon <Artem_Tarasenko-Syvokon@epam.com>
2020-02-22 19:18:15 +0200

    Lesson 16. Refactoring

--- src/components/Profile/MyPost/Post/Post.jsx
+++ src/components/Profile/MyPost/Post/Post.jsx
@@ -7,7 +7,7 @@ const Post = (props) => {
       <img src="https://yt3.ggpht.com/a/AGF-l7-uueDdRmZsJQOVmDZCeIjv8tU9swZd1pJYCw=s900-c-k-c0xffffffff-no-rj-mo" />
       {props.message}
       <div>
-        <span>{props.likesCount}</span>
+        <span>likes {props.likesCount}</span>
       </div>
     </div>
   );
