eb8176e3ff7d07e979123b18551ad8a56746053b
Artem Tarasenko-Syvokon <Artem_Tarasenko-Syvokon@epam.com>
2020-03-04 22:12:25 +0200

    Lesson 22. Dialogs updated to contains links as names of users

--- src/components/Dialog/Dialogs.jsx
+++ src/components/Dialog/Dialogs.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import styles from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Dialogs = () => {
   return (
     <div className={styles.dialogs}>
       <div className={styles.dialogItems}>
-        <div className={styles.dialog + " " + styles.active}>Nastiia</div>
-        <div className={styles.dialog}>Andrew</div>
-        <div className={styles.dialog}>Dennis</div>
-        <div className={styles.dialog}>Alexei</div>
-        <div className={styles.dialog}>Artem</div>
+        <div className={styles.dialog + " " + styles.active}>
+          <NavLink to="/dialogs/1">Nastiia</NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to="/dialogs/2">Andrew</NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to="/dialogs/3">Dennis</NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to="/dialogs/4">Alexei</NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to="/dialogs/5">Artem</NavLink>
+        </div>
       </div>
       <div className={styles.messages}>
         <div className={styles.message}>Hello, how are you?</div>
