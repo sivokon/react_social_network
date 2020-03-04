5b7df25c76484556ad54a7e7a51d5cd5314d17dc
Artem Tarasenko-Syvokon <Artem_Tarasenko-Syvokon@epam.com>
2020-03-04 21:31:54 +0200

    Lesson 21. Working on dialogs page

--- src/components/Dialog/Dialogs.jsx
+++ src/components/Dialog/Dialogs.jsx
@@ -3,8 +3,19 @@ import styles from './Dialogs.module.css';
 
 const Dialogs = () => {
   return (
-    <div>
-      Dialogs
+    <div className={styles.dialogs}>
+      <div className={styles.dialogItems}>
+        <div className={styles.dialog + " " + styles.active}>Nastiia</div>
+        <div className={styles.dialog}>Andrew</div>
+        <div className={styles.dialog}>Dennis</div>
+        <div className={styles.dialog}>Alexei</div>
+        <div className={styles.dialog}>Artem</div>
+      </div>
+      <div className={styles.messages}>
+        <div className={styles.message}>Hello, how are you?</div>
+        <div className={styles.message}>Let's go to gym.</div>
+        <div className={styles.message}>Have you done EO lab?</div>
+      </div>
     </div>
   );
 }
