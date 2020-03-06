f2c422dddd88a1f356f83a839baaabb97651c68e
Artem Tarasenko-Syvokon <Artem_Tarasenko-Syvokon@epam.com>
2020-03-06 23:47:12 +0200

    Lesson 24. Data extracted to the objects (inside the components)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,19 +4,34 @@ import Dialog from './DialogItem/DialogItem';
 import Message from './Message/Message';
 
 const Dialogs = () => {
+
+  let users = [
+    { id: 1, name: "Dima" },
+    { id: 2, name: "Andrew" },
+    { id: 3, name: "Dennis" },
+    { id: 4, name: "Alexei" },
+    { id: 5, name: "Artem" }
+  ];
+
+  let messages = [
+    { id: 1, message: "Hello, how are you?" },
+    { id: 2, message: "Let's go to gym." },
+    { id: 3, message: "Have you done EO lab?" }
+  ];
+
   return (
     <div className={styles.dialogs}>
       <div className={styles.dialogItems}>
-        <Dialog name="Nastiia" id="1" />
-        <Dialog name="Andrew" id="2" />
-        <Dialog name="Dennis" id="3" />
-        <Dialog name="Alexei" id="4" />
-        <Dialog name="Artem" id="5" />
+        <Dialog name={users[0].name} id={users[0].id} />
+        <Dialog name={users[1].name} id={users[1].id} />
+        <Dialog name={users[2].name} id={users[2].id} />
+        <Dialog name={users[3].name} id={users[3].id} />
+        <Dialog name={users[4].name} id={users[4].id} />
       </div>
       <div className={styles.messages}>
-        <Message message="Hello, how are you?"/>
-        <Message message="Let's go to gym."/>
-        <Message message="Have you done EO lab?"/>
+        <Message message={messages[0].message}/>
+        <Message message={messages[1].message}/>
+        <Message message={messages[2].message}/>
       </div>
     </div>
   );
--- src/components/Profile/MyPost/MyPost.jsx
+++ src/components/Profile/MyPost/MyPost.jsx
@@ -3,6 +3,13 @@ import styles from './MyPost.module.css';
 import Post from './Post/Post';
 
 const MyPost = () => {
+
+  let posts = [
+    { id:1, message: "Hello! I am learning React.", likesCount: 5 },
+    { id:2, message: "Look, today is a good day.", likesCount: 7 },
+    { id:3, message: "How about reading some article?", likesCount: 10 }
+  ];
+
   return (
     <div className={styles.postsBlock}>
       <h3>my posts</h3>
@@ -15,9 +22,9 @@ const MyPost = () => {
         </div>
       </div>
       <div className={styles.posts}>
-        <Post message="Hello! I am learning React." likesCount="5" />
-        <Post message="Look, today is a good day." likesCount="7" />
-        <Post message="How about reading some article?" likesCount="10" />
+        <Post message={posts[0].message} likesCount={posts[0].likesCount} />
+        <Post message={posts[1].message} likesCount={posts[1].likesCount} />
+        <Post message={posts[2].message} likesCount={posts[2].likesCount} />
       </div>
     </div>
   );
