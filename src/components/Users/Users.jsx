760802989017aae25a164f4976a6b27fd1b7ad8e
Artem Tarasenko-Syvokon <Artem_Tarasenko-Syvokon@epam.com>
2020-07-02 23:11:07 +0300

    Lesson 53. Users component rewritten to be class component.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,29 +3,32 @@ import User from './User/User';
 import styles from './Users.module.css';
 import * as axios from 'axios';
 
-const Users = (props) => {
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
 
-  if (!props.users.length) {
     axios.get("https://social-network.samuraijs.com/api/1.0/users")
       .then( response => props.setUsers(response.data.items) );
   }
-  
-  let users = props.users.map(
-    user => <User user={user} toggleFollow={props.toggleFollow}/>);
 
-  return (
-    <div class={styles.usersPage}>
-      <div>
+  render() {
+    let users = this.props.users.map(
+      user => <User user={user} toggleFollow={this.props.toggleFollow}/>);
+
+    return (
+      <div class={styles.usersPage}>
         <div>
-          {users}
+          <div>
+            {users}
+          </div>
+          <div className={styles.loadBtn}>
+            <button>Show More</button>
+          </div>
         </div>
-        <div className={styles.loadBtn}>
-          <button>Show More</button>
-        </div>
+        <div></div>
       </div>
-      <div></div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Users;
