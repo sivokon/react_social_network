ceb346e74cb6feaf5f63cf9c5787654e16ac3535
Artem Tarasenko-Syvokon <Artem_Tarasenko-Syvokon@epam.com>
2020-07-06 02:11:08 +0300

    Refactoring

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,13 +14,13 @@ const Navbar = () => {
         <NavLink to="/dialogs" activeClassName={styles.active}>Messages</NavLink>
       </div>
       <div className={styles.item}>
+        <NavLink to="/users" activeClassName={styles.active}>Users</NavLink>
+      </div>
+      <div className={styles.item}>
         <NavLink to="/news" activeClassName={styles.active}>News</NavLink>
       </div>
       <div className={styles.item}>
         <NavLink to="/music" activeClassName={styles.active}>Music</NavLink>
-      </div>
-      <div className={styles.item}>
-        <NavLink to="/users" activeClassName={styles.active}>Users</NavLink>
       </div>
       <div className={styles.item}>
         <NavLink to="/settings" activeClassName={styles.active}>Settings</NavLink>
