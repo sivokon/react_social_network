f8186323feb7c7bbc3832b3600ce128116cd1d3e
Artem Tarasenko-Syvokon <Artem_Tarasenko-Syvokon@epam.com>
2020-07-05 13:07:43 +0300

    Lesson 54. componentDidMount added to the Users component (to make ajax request).

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,11 +4,10 @@ import styles from './Users.module.css';
 import * as axios from 'axios';
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
 
+  componentDidMount() {
     axios.get("https://social-network.samuraijs.com/api/1.0/users")
-      .then( response => props.setUsers(response.data.items) );
+      .then( response => this.props.setUsers(response.data.items) );
   }
 
   render() {
