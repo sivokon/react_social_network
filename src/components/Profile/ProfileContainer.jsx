b492400124c0dc48c2f53fceeb2c6b218317359e
Artem Tarasenko-Syvokon <Artem_Tarasenko-Syvokon@epam.com>
2020-07-13 00:13:34 +0300

    Lesson 60. withRouter HOC used to get userId from the URL.

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,11 +3,14 @@ import Profile from './Profile';
 import * as axios from 'axios';
 import { connect } from 'react-redux';
 import { setUserProfile } from '../../redux/profileReducer';
+import { withRouter } from 'react-router-dom';
 
 class ProfileContainer extends React.Component {
 
   componentDidMount() {
-    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+    let userId = this.props.match.params.userId || 2;
+
+    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
       .then(response => {
         this.props.setUserProfile(response.data);
       });
@@ -24,4 +27,6 @@ let mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
+let ProfileContainerWithUrlParams = withRouter(ProfileContainer);
+
+export default connect(mapStateToProps, { setUserProfile })(ProfileContainerWithUrlParams);
